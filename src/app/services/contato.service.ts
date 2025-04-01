5edb70ff15e4f381032c2a0d47c293ba27425eb7
saeleuterio <saeleuterio@hotmail.com>
2025-04-01 17:08:54 -0300

    up

---
+++ src/app/services/contato.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Contato } from '../componentes/contato/contato';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContatoService {
+
+  private readonly API = 'http://localhost:3000/contatos';
+
+  constructor(private http: HttpClient) {}
+
+  obterContatos(): Observable<Contato[]> {
+    return this.http.get<Contato[]>(this.API);
+  }
+
+  salvarContato(contato: Contato) {
+    return this.http.post<Contato>(this.API, contato)
+  }
+}
