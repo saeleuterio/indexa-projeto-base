15a9f568b4800630d2def33eed9c2f0811dc6f01
saeleuterio <saeleuterio@hotmail.com>
2025-04-02 20:04:47 -0300

    up

--- src/app/paginas/lista-contatos/lista-contatos.component.ts
+++ src/app/paginas/lista-contatos/lista-contatos.component.ts
@@ -10,6 +10,7 @@ import { SeparadorComponent } from '../../componentes/separador/separador.compon
 import { FormularioContatoComponent } from '../formulario-contato/formulario-contato.component';
 import { ContatoService } from '../../services/contato.service';
 import { Contato } from '../../componentes/contato/contato';
+import { PerfilContatoComponent } from "../perfil-contato/perfil-contato.component";
 
 @Component({
   selector: 'app-lista-contatos',
@@ -22,8 +23,9 @@ import { Contato } from '../../componentes/contato/contato';
     ContatoComponent,
     FormsModule,
     FormularioContatoComponent,
-    RouterLink
-  ],
+    RouterLink,
+    PerfilContatoComponent
+],
   templateUrl: './lista-contatos.component.html',
   styleUrl: './lista-contatos.component.css'
 })
